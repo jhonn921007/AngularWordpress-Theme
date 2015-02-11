aec1b8f4bde345f879ee97d052786db86c941d8a
JhonnatanRodriguez <jhonn@outlook.com>
2015-02-11 13:04:35 -0400

    add navbar, content y footer

--- header.php
+++ header.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> ng-app="myapp">
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
   <title>Mi AngularJS Tema</title>
   <?php wp_head();?>
+
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <?php get_header(); ?>
-<div ng-controller="mycontroller">
-  <!-- display all post titles in a list -->
-  <ul>
-    <li ng-repeat="post in postdata">{{post.title}}</li>
-  </ul>
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Name Project</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <!-- <li><a href="#about">About</a></li> -->
+
+
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
+
+<!-- Begin page content -->
+<div class="container">
+
+  <div ng-controller="mycontroller">
+    <!-- display all post titles in a list -->
+    <ul>
+      <li ng-repeat="post in postdata">{{post.title}}</li>
+    </ul>
+  </div>
+
 </div>
+
+<footer class="footer">
+  <div class="container">
+    <p class="text-muted">SMS Voice Free © 2015 All Rights Reserved.</p>
+  </div>
+</footer>
+
+
+
 <?php get_footer(); ?>
